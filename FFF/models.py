99b2821627d04cfc36d245b1f0712e3dd5bb0584
Omkar Moghe <omoghe@umich.edu>
2017-06-03 18:24:14 -0700

    basic model stuff, still needs work

--- FFF/models.py
+++ FFF/models.py
@@ -3,3 +3,20 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class ItemType(models.Model):
+    name = models.CharField(max_length=128, db_index=True)
+    cost = models.DecimalField(max_digits=7, decimal_places=2)
+    tags = models.ManyToManyField(Category)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=32, db_index=True)
+
+
+class Item(models.Model):
+    id = models.CharField(max_length=32, unique=True, db_index=True)
+    item_type = models.ForeignKey('ItemType', null=False, blank=False)
+    location = models.CharField(max_length=128)  # TODO
+    quantity = models.IntegerField()
